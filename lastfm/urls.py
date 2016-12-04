fc05c42a85d6e9059cc0273a1dba6feef1c8cc12
Guillaume Lafranceschina <contact@guillaume.pro>
2016-12-04 05:49:22 +0100

    Import des URL plus clean

--- lastfm/urls.py
+++ lastfm/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
+from views import *
 
 urlpatterns = [
-    url(r'^connect/', views.lastfm_connect, name='lastfm_connect'),
-    url(r'^unlink/', views.lastfm_unlink, name='lastfm_unlink'),
-    url(r'^callback/', views.lastfm_callback, name='lastfm_callback'),
-    url(r'^annual_summary/', views.lastfm_annual_summary, name='lastfm_annual_summary'),
-    url(r'^synchronise/(?P<period>\S+)/', views.lastfm_synchronise, name='lastfm_synchronise'),
+    url(r'^connect/', lastfm_connect, name='lastfm_connect'),
+    url(r'^unlink/', lastfm_unlink, name='lastfm_unlink'),
+    url(r'^callback/', lastfm_callback, name='lastfm_callback'),
+    url(r'^annual_summary/', lastfm_annual_summary, name='lastfm_annual_summary'),
+    url(r'^synchronise/(?P<period>\S+)/', lastfm_synchronise, name='lastfm_synchronise'),
 ]
